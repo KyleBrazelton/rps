3d7c294d30c8bf8aee0ad0072258e6148ec18910
kyle brazelton <kylejbrazelton@gmail.com>
2022-05-14 12:08:43 -0700

    Detail js functions with psuedocode

---
+++ rps.js
@@ -0,0 +1,17 @@
+//Create a function called computerPlay
+//randomly return either 'rock', 'paper', or 'scissors'
+//use console to check output
+
+//write function that plays single round of rock paper scissors
+//require two parameters
+//playerSelection
+//computerSelection
+//return a string that declares a winner - You lose! Paper Beats Rock! for each 
+//case insensitivity on the user input
+//return results of function call
+
+//write a function called game()
+//call playRound function inside of this to play a 5 round game that keeps score/reports
+//Use a for loop for playRound function
+//console.log to display results of each round and winner
+//use prompt() to get user input
